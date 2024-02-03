24ebb1a948f0a6ad452240150e84ac7e189fe03c
Jhonny Varela <jhonny_varela_visbal@hotmail.com>
2024-02-03 14:36:20 -0400

    adding .env support

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -10,7 +9,6 @@ import (
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
-		fmt.Println(err)
 		panic(err)
 	}
 
