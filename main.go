026e1d6bea8f947adb38c470d506c49fd409c8d4
Jhonny Varela <jhonny_varela_visbal@hotmail.com>
2024-02-06 17:43:04 -0400

    adding a couple of things

--- main.go
+++ main.go
@@ -1,56 +1,65 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+func getEnvVar(key string) string {
+	err := godotenv.Load(".env")
+
 	if err != nil {
-		panic(err)
+    log.Panic(err)
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(getEnvVar("TELEGRAM_API_TOKEN"))
+	if err != nil {
+		log.Panic(err)
 	}
 
 	bot.Debug = true
-	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
-	// to make sure Telegram knows we've handled previous values and we don't
-	// need them repeated.
-	updateConfig := tgbotapi.NewUpdate(0)
 
-	// Tell Telegram we should wait up to 30 seconds on each request for an
-	// update. This way we can get information just as quickly as making many
-	// frequent requests without having to send nearly as many.
-	updateConfig.Timeout = 30
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	// Start polling Telegram for updates.
-	updates := bot.GetUpdatesChan(updateConfig)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-	// Let's go through each update that we're getting from Telegram.
+	updates := bot.GetUpdatesChan(u)
+
 	for update := range updates {
-		// Telegram can send many types of updates depending on what your Bot
-		// is up to. We only want to look at messages for now, so we can
-		// discard any other updates.
-		if update.Message == nil {
+		if update.Message == nil { // ignore any non-Message updates
 			continue
 		}
 
-		// Now that we know we've gotten a new message, we can construct a
-		// reply! We'll take the Chat ID and Text from the incoming message
-		// and use it to create a new message.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		// We'll also say that this message is a reply to the previous message.
-		// For any other specifications than Chat ID or Text, you'll need to
-		// set fields on the `MessageConfig`.
-		msg.ReplyToMessageID = update.Message.MessageID
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
 
-		// Okay, we're sending our message off! We don't care about the message
-		// we just sent, so we'll discard it.
+		// Create a new MessageConfig. We don't have text yet,
+		// so we leave it empty.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /sayhi and /status."
+		case "sayhi":
+			msg.Text = "Hi :)"
+		case "status":
+			msg.Text = "I'm ok."
+		default:
+			msg.Text = "I don't know that command"
+		}
+
 		if _, err := bot.Send(msg); err != nil {
-			// Note that panics are a bad way to handle errors. Telegram can
-			// have service outages or network errors, you should retry sending
-			// messages or more gracefully handle failures.
-			panic(err)
+			log.Panic(err)
 		}
 	}
 }
